b61ed3170a4f569c5ef860df1d99ef7692ec1e7a
romain-vitalis <romain.vitalis@laplateforme.iO>
2022-03-15 13:45:58 +0100

    job01

---
+++ jour02/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>runtrack3 jour02 job01</title>
+    <script type="text/javascript" src="script.js">
+    </script>
+  </head>
+  <body>
+    <article id="citation">
+      <p>La vie a beaucoup plus d'imagination que nous</p>
+    </article>
+    <input type="button" name="button" value="Citation" onclick="citation();">
+  </body>
+</html>
