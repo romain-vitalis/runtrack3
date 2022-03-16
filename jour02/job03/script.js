0743d71a807f07fb02e6b00590dd3f673e914869
romain-vitalis <romain.vitalis@laplateforme.iO>
2022-03-16 09:22:47 +0100

    job03

---
+++ jour02/job03/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function addone() {
+
+        let compt = document.getElementById("compteur").innerHTML;
+        document.getElementById("compteur").innerHTML = ++compt;
+    }
+
+    let bouton = document.querySelector('#button')
+    bouton.addEventListener("click", (e) => {
+        addone()
+        console.log(addone)
+    })
+    }
+
+)
