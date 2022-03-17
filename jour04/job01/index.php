75da0ee85c07a6adafbf2eb659c50d2ec2cd3a37
romain-vitalis <romain.vitalis@laplateforme.iO>
2022-03-17 17:06:01 +0100

    job01

---
+++ jour04/job01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>runtrack 3 jour04 Job 01</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <button id="button">Fetch</button>
+    
+</body>
+</html>
