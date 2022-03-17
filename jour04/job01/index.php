d4775986d5c42ef6054153edbde6b22f3c6062d3
romain-vitalis <romain.vitalis@laplateforme.iO>
2022-03-17 16:58:49 +0100

    job 01

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Runtrack jour 01 Job 01</title>
+    <title>Runtrack jour 04 Job 01</title>
     <script src="script.js"></script>
 </head>
 <body>
