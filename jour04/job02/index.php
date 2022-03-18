a5743bcfbcc1ce463788d26a7b87cf37f0e3cc58
romain-vitalis <romain.vitalis@laplateforme.iO>
2022-03-18 12:02:53 +0100

    job02

---
+++ jour04/job02/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job02</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
