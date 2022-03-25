e485b507dd721af70d160eb76208fad9fa0095ab
romain-vitalis <romain.vitalis@laplateforme.iO>
2022-03-25 08:49:53 +0100

    jour08

---
+++ jour08/job01/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>runtrack 3 job 01</title>
+</head>
+<body>
+    <header>
+<div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+
+        <ul class="right hide-on-med-and-down">
+        <li><a href="#connexion">connexion</a></li>
+        <li><a href="#inscription">inscription</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  </header>
+  <main>
+    <section>
+    <form action="#">
+        <h3>Civilité <i class="small material-icons">people</i></h3>
+    <p>
+      <label>
+        <input name="group1" type="radio"/>
+        <span>Mr</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input name="group1" type="radio" />
+        <span>Mme</span>
+      </label>
+    </p>
+    <div class="row">
+        <div class="input-field col s6">
+          <input id="first_name" type="text" class="validate">
+          <label for="first_name">First Name <i class="tiny material-icons">sentiment_very_satisfied</i></label>
+        </div>
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">Last Name <i class="tiny material-icons">sentiment_very_satisfied</i></label>
+        </div>
+        <div class="input-field col s6">
+          <input id="address" type="text" class="validate">
+          <label for="address">Address <i class="tiny material-icons">place</i></label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email <i class="tiny material-icons">email</i></label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Password <i class="tiny material-icons">offline_pin</i></label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Confirm your password <i class="tiny material-icons">offline_pin</i></label>
+        </div>
+      </div>
+      <h3>Choisissez vos passions <i class="medium material-icons">favorite</i></h3>
+      <p>
+      <label>
+        <input type="checkbox" class="filled-in"/>
+        <span>Sport</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input type="checkbox" class="filled-in"/>
+        <span>Musique</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input type="checkbox" class="filled-in"/>
+        <span>Cinéma</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input type="checkbox" class="filled-in"/>
+        <span>Cuisine</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input type="checkbox" class="filled-in"/>
+        <span>Informatique</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input type="checkbox" class="filled-in"/>
+        <span>Théatre</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input type="checkbox" class="filled-in"/>
+        <span>Peinture</span>
+      </label>
+    </p>
+    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+  </button>
+  </form>
+    </section>
+  </main>
+  <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l4 offset-l5 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Accueil</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Inscription</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Connexion</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Rechercher</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2022 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+<script type="text/javascript" src="js/materialize.min.js"></script>    
+</body>
+</html>
